f274f5e54236217ace47026c5ccbd1f7765d1c76
Anna Bykadorova <ASBykadorova@sberbank.ru>
2022-04-29 16:47:52 +0300

    module1-task1 | анимация для навигации, появление текста

--- source/js/modules/social.js
+++ source/js/modules/social.js
@@ -1,9 +1,18 @@
 export default () => {
-  let socialBlock = document.querySelector(`.js-social-block`);
+  const socialBlock = document.querySelector(`.js-social-block`);
   socialBlock.addEventListener(`mouseover`, function () {
     socialBlock.classList.add(`social-block--active`);
   });
   socialBlock.addEventListener(`mouseleave`, function () {
     socialBlock.classList.remove(`social-block--active`);
+  });
+
+  const socialList = document.querySelector(`.js-social-list`);
+  const socialToggler = document.querySelector(`.js-social-toggler`);
+  socialList.addEventListener(`mouseover`, function () {
+    socialToggler.classList.add(`social-block__toggler--active`);
+  });
+  socialList.addEventListener(`mouseleave`, function () {
+    socialToggler.classList.remove(`social-block__toggler--active`);
   });
 };
